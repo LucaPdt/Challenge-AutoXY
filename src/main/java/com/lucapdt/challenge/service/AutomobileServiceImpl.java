15cdaa84b431a925419cd3c026d95f190206fd2e
LucaPdt <pedottiluca@gmail.com>
2025-02-10 11:50:56 +0100

    feat-green: findAll con paging
    - Aggiunto campo @Autowired di automobileRepository nel service
    - Implementato metodo (Page) findAll()

--- src/main/java/com/lucapdt/challenge/service/AutomobileServiceImpl.java
+++ src/main/java/com/lucapdt/challenge/service/AutomobileServiceImpl.java
@@ -1,14 +1,20 @@
 package com.lucapdt.challenge.service;
 
 import com.lucapdt.challenge.entity.Automobile;
+import com.lucapdt.challenge.repository.AutomobileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 
 public class AutomobileServiceImpl implements AutomobileService{
 
+    @Autowired
+    private AutomobileRepository automobileRepository;
+
     @Override
     public Page<Automobile> findAll(int page, int size) {
-        return null;
+        return automobileRepository.findAll(PageRequest.of(page, size));
     }
 }
